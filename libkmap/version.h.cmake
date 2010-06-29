f71c978fedbf362d0221a930e5754345e5b5df1f
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-29 13:01:36 +0000

    implement pure libkmap library as libkexiv2, libkface, and libkdcraw
    svn path=/branches/extragear/graphics/digikam/libkmap/; revision=1144174

---
+++ libkmap/version.h.cmake
@@ -0,0 +1,32 @@
+/** ===========================================================
+ *
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ *
+ * @date   2010-06-29
+ * @brief  world maps library interface for KDE
+ *
+ * @author Copyright (C) 2010 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef KMAP_VERSION_H
+#define KMAP_VERSION_H
+
+static const char kmap_version[] = "${KMAP_LIB_VERSION_STRING}";
+
+#define KMAP_VERSION ${KMAP_LIB_VERSION_ID}
+
+#endif // KMAP_VERSION_H
