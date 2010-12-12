b743664b5a2f36cc7e06ed50da6c5982af1f3a4e
Michael Georg Hansen <mike@mghansen.de>
2010-12-12 19:54:35 +0000

    Make sure the local versions of libkmap and libkface are found: - add a KMAP_LOCAL_DIR and KFACE_LOCAL_DIR variable to allow local directories other than '/' and '/libs' - add the build directory as an include directory, otherwise version.h can not be found. The only drawback to this is that now XXX_INCLUDE_DIR contains two directory names.
    svn path=/branches/extragear/graphics/digikam/3rdparty/libkmap/; revision=1205847

--- cmake/modules/FindKmap.cmake
+++ cmake/modules/FindKmap.cmake
@@ -1,4 +1,8 @@
 # - Try to find the KMap library
+#
+# If you have put a local version of libkmap into your source tree,
+# set KMAP_LOCAL_DIR to the relative path to the local directory.
+#
 # Once done this will define
 #
 #  KMAP_FOUND - system has libkmap
@@ -12,34 +16,44 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-IF (KMAP_INCLUDE_DIR AND KMAP_LIBRARIES)
+IF (KMAP_INCLUDE_DIR AND KMAP_LIBRARIES AND KMAP_DEFINITIONS)
 
   MESSAGE(STATUS "Found Kmap library in cache: ${KMAP_LIBRARIES}")
 
   # in cache already
   SET(KMAP_FOUND TRUE)
 
-ELSE (KMAP_INCLUDE_DIR AND KMAP_LIBRARIES)
+ELSE (KMAP_INCLUDE_DIR AND KMAP_LIBRARIES AND KMAP_DEFINITIONS)
 
   MESSAGE(STATUS "Check Kmap library in local sub-folder...")
 
   # Check if library is not in local sub-folder
 
-  FIND_FILE(KMAP_LOCAL_FOUND libkmap/version.h.cmake ${CMAKE_SOURCE_DIR}/libkmap ${CMAKE_SOURCE_DIR}/libs/libkmap NO_DEFAULT_PATH)
+  IF (KMAP_LOCAL_DIR)
+   SET(KMAP_LOCAL_FOUND TRUE)
+  ELSE (KMAP_LOCAL_DIR)
+    FIND_FILE(KMAP_LOCAL_FOUND libkmap/version.h.cmake ${CMAKE_SOURCE_DIR}/libkmap ${CMAKE_SOURCE_DIR}/libs/libkmap NO_DEFAULT_PATH)
+
+    IF (KMAP_LOCAL_FOUND)
+      FIND_FILE(KMAP_LOCAL_FOUND_IN_LIBS libkmap/version.h.cmake ${CMAKE_SOURCE_DIR}/libs/libkmap NO_DEFAULT_PATH)
+        IF (KMAP_LOCAL_FOUND_IN_LIBS)
+          SET(KMAP_LOCAL_DIR libs/libkmap)
+        ELSE (KMAP_LOCAL_FOUND_IN_LIBS)
+          SET(KMAP_LOCAL_DIR libkmap)
+        ENDIF (KMAP_LOCAL_DIR)
+    ENDIF (KMAP_LOCAL_FOUND)
+  ENDIF (KMAP_LOCAL_DIR)
 
   IF (KMAP_LOCAL_FOUND)
 
-    FIND_FILE(KMAP_LOCAL_FOUND_IN_LIBS libkmap/version.h.cmake ${CMAKE_SOURCE_DIR}/libs/libkmap NO_DEFAULT_PATH)
-    IF (KMAP_LOCAL_FOUND_IN_LIBS)
-      SET(KMAP_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/libs/libkmap)
-    ELSE (KMAP_LOCAL_FOUND_IN_LIBS)
-      SET(KMAP_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/libkmap)
-    ENDIF (KMAP_LOCAL_FOUND_IN_LIBS)
-    SET(KMAP_DEFINITIONS "-I${KMAP_INCLUDE_DIR}")
+    # we need two include directories: because the version.h file is put into the build directory
+    # TODO KMAP_INCLUDE_DIR sounds like it should contain only one directory...
+    SET(KMAP_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/${KMAP_LOCAL_DIR} ${CMAKE_BUILD_DIR}/${KMAP_LOCAL_DIR})
+    SET(KMAP_DEFINITIONS "-I${CMAKE_SOURCE_DIR}/${KMAP_LOCAL_DIR}" "-I${CMAKE_BUILD_DIR}/${KMAP_LOCAL_DIR}")
     SET(KMAP_LIBRARIES kmap)
-    MESSAGE(STATUS "Found Kmap library in local sub-folder: ${KMAP_INCLUDE_DIR}")
+    MESSAGE(STATUS "Found Kmap library in local sub-folder: ${CMAKE_SOURCE_DIR}/${KMAP_LOCAL_DIR}")
     SET(KMAP_FOUND TRUE)
-    MARK_AS_ADVANCED(KMAP_INCLUDE_DIR KMAP_LIBRARIES)
+    MARK_AS_ADVANCED(KMAP_INCLUDE_DIR KMAP_LIBRARIES KMAP_DEFINITIONS)
 
   ELSE(KMAP_LOCAL_FOUND)
     IF(NOT WIN32) 
@@ -101,8 +115,8 @@ ELSE (KMAP_INCLUDE_DIR AND KMAP_LIBRARIES)
           ENDIF (Kmap_FIND_REQUIRED)
       ENDIF (KMAP_FOUND)
 
-    MARK_AS_ADVANCED(KMAP_INCLUDE_DIR KMAP_LIBRARIES)
+    MARK_AS_ADVANCED(KMAP_INCLUDE_DIR KMAP_LIBRARIES KMAP_DEFINITIONS)
 
   ENDIF(KMAP_LOCAL_FOUND)
 
-ENDIF (KMAP_INCLUDE_DIR AND KMAP_LIBRARIES)
+ENDIF (KMAP_INCLUDE_DIR AND KMAP_LIBRARIES AND KMAP_DEFINITIONS)
