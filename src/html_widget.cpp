fd89338d694afaaf0969a320da8f1040bc5b77f9
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2015-03-01 17:51:14 +0200

    Polishing

--- src/html_widget.cpp
+++ src/html_widget.cpp
@@ -11,6 +11,8 @@
  *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
  * @author Copyright (C) 2010-2015 by Gilles Caulier
  *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ * @author Copyright (C) 2015      by Mohamed Anwer
+ *         <a href="mailto:mohammed.ahmed.anwer@gmail.com">mohammed dot ahmed dot anwer at gmail dot com</a>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -49,7 +51,6 @@ public:
     Private()
       : parent(0),
         isReady(false),
-        //javascriptScanTimer(0),
         selectionStatus(false),
         firstSelectionPoint(),
         intermediateSelectionPoint(),
@@ -60,7 +61,6 @@ public:
 
     QWidget*       parent;
     bool           isReady;
-    //QTimer*        javascriptScanTimer;
 
     bool           selectionStatus;
     GeoCoordinates firstSelectionPoint;
@@ -78,11 +78,6 @@ HTMLWidget::HTMLWidget(QWidget* const parent)
     setRenderHint(QPainter::TextAntialiasing);
 
     d->parent->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-    // create a timer for monitoring for javascript events, but do not start it yet:
-//    d->javascriptScanTimer = new QTimer(this);
-//    d->javascriptScanTimer->setSingleShot(false);
-//    d->javascriptScanTimer->setInterval(300);
 
     connect(this, SIGNAL(loadProgress(int)),
             this, SLOT(progress(int)));
@@ -104,35 +99,22 @@ HTMLWidget::~HTMLWidget()
     delete d;
 }
 
-// This method wasn't called ever, before porting to QWebView
-// What does it do?
-//void HTMLWidget::loadInitialHTML(const QString& initialHTML)
-//{
-////     qCDebug(LIBKGEOMAP_LOG) << initialHTML;
-//    begin();
-//    write(initialHTML);
-//    end();
-//}
-
 void HTMLWidget::progress(int progress)
 {
-    qDebug() << progress;
+    qCDebug(LIBKGEOMAP_LOG) << "Maps Loading Progress: " << progress << "%";
 }
 
 void HTMLWidget::slotHTMLCompleted(bool ok)
 {
-    qDebug() << ok;
+    qCDebug(LIBKGEOMAP_LOG) << "Map Loading Completed: " << ok;
     d->isReady = ok;
-
-    // start monitoring for javascript events using a timer:
-    // d->javascriptScanTimer->start();
 
     emit(signalJavaScriptReady());
 }
 
 void HTMLWidget::mousePressEvent(QMouseEvent* e)
 {
-//    slotScanForJSMessages();
+    slotScanForJSMessages(QString::fromLatin1("(event)"));
     QWebView::mousePressEvent(e);
 }
 
@@ -201,7 +183,7 @@ void HTMLWidget::mouseReleaseEvent(QMouseEvent* e)
         }
     }
 
-//    slotScanForJSMessages();
+    slotScanForJSMessages(QString::fromLatin1("(event)"));
     QWebView::mouseReleaseEvent(e);
 }
 
@@ -250,7 +232,6 @@ void HTMLWidget::mouseMoveEvent(QMouseEvent *e)
                   .arg(latSouth));
     }
 
-//    slotScanForJSMessages();
     QWebView::mouseMoveEvent(e);
 }
 
@@ -259,7 +240,7 @@ void HTMLWidget::slotScanForJSMessages(QString message)
     if (message!=QLatin1String("(event)"))
         return;
 
-    qCDebug(LIBKGEOMAP_LOG) << message;
+//    qCDebug(LIBKGEOMAP_LOG) << message;
 
     const QString eventBufferString = runScript(QLatin1String("kgeomapReadEventStrings();")).toString();
 
--- src/html_widget.h
+++ src/html_widget.h
@@ -11,6 +11,8 @@
  *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
  * @author Copyright (C) 2010-2015 by Gilles Caulier
  *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ * @author Copyright (C) 2015      by Mohamed Anwer
+ *         <a href="mailto:mohammed.ahmed.anwer@gmail.com">mohammed dot ahmed dot anwer at gmail dot com</a>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
