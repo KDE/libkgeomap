6c576e957f48b1508c3b06e24f02ef5e84d07efe
Alexander Potashev <aspotashev@gmail.com>
2015-09-17 11:06:46 +0200

    GroupStateComputer: cleanup

--- src/groupstatecomputer.cpp
+++ src/groupstatecomputer.cpp
@@ -5,7 +5,7 @@
  * <a href="http://www.digikam.org">http://www.digikam.org</a>
  *
  * @date   2009-12-01
- * @brief  Primitive datatypes for KGeoMap
+ * @brief  class GroupStateComputer
  *
  * @author Copyright (C) 2009-2010 by Michael G. Hansen
  *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
@@ -29,7 +29,6 @@
 #include "groupstatecomputer.h"
 #include "types.h"
 #include "libkgeomap_debug.h"
-// #include "geocoordinates.h"
 
 namespace KGeoMap
 {
